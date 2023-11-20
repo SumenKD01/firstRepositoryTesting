5c4eeafc2a0c78efb39f9bf086b2e0ea67bdd715
Sumen <sumen@ehs4safety.com>
2023-11-21 01:59:06 +0530

    Made an HTML file and Practiced DOM through getElementById, and finally pushed inside the Git

--- Test1.js
+++ Test1.js
@@ -1,5 +1,22 @@
-function Test1() {
-    console.log("Hello World!");
-}
-//This is just for the Git Task given by Sharpener.
-Test1();
+// Examine the document object
+
+// console.dir(document);
+
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.doctype);
+// console.log(document.title);
+// console.log(document.head);
+
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// document.all[10].textContent = "hello Sumen";
+// console.log(document.forms);
+// console.log(document.forms[0]);
+// console.log(document.links);
+// console.log(document.images);
+
+const titleBar = document.getElementById('main-header');
+
+titleBar.style.borderBottom = 'solid 2px black'
