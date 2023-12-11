3dae1222184a9e1586598a74eba7190b7bbc3b25
Sumen Kumar Dubey <sumenkumard@gmail.com>
2023-12-11 23:17:10 +0530

    Completed the task again

--- Test1.js
+++ Test1.js
@@ -32,13 +32,14 @@ function addItem(e) {
 
     // Get input value
     var newItem = document.getElementById('item').value;
+    var newItemDescription = document.getElementById('description').value;
 
     // Create new li element
     var li = document.createElement('li');
     // Add class
     li.className = 'list-group-item';
     // Add text node with input value
-    li.appendChild(document.createTextNode(newItem));
+    li.appendChild(document.createTextNode(newItem+" "+newItemDescription));
 
     // Create del button element
     var deleteBtn = document.createElement('button');
