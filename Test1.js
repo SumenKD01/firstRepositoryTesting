ccb3587b1e59aaa31d64674184f035a9a780315e
Sumen Kumar Dubey <sumenkumard@gmail.com>
2023-12-13 22:26:28 +0530

    Made the edit functionality

--- Test1.js
+++ Test1.js
@@ -5,10 +5,16 @@ var submitButton = document.getElementById('submitButton');
 form.addEventListener('submit', addToStorage);
 
 function deleteFunctionality(e) {
-    console.log(e.target.parentElement);
     e.target.parentElement.style.display = 'none';
-    console.log("Email - ",e.target.parentElement.getAttribute('email'));
     localStorage.removeItem(e.target.parentElement.getAttribute('email'));
+}
+
+function editFunctionlity(e){
+    e.target.parentElement.style.display = 'none';
+    localStorage.removeItem(e.target.parentElement.getAttribute('email'));
+    document.getElementById('name').value = e.target.parentElement.getAttribute('name');
+    document.getElementById('email').value = e.target.parentElement.getAttribute('email');
+    document.getElementById('phone').value = e.target.parentElement.getAttribute('phone');
 }
 
 function addToStorage(e) {
@@ -24,16 +30,28 @@ function addToStorage(e) {
     // console.log(nameGiven, age);
     var allUserDetailList = document.getElementById('allUserDetails');
     let newDetail = document.createElement("li");
+
     let deleteButton = document.createElement("input");
     deleteButton.type = 'button';
     deleteButton.value = "delete";
-    console.log(newDetail);
+
+    let editButton = document.createElement("input");
+    editButton.type = 'button';
+    editButton.value = "edit";
+
+    // console.log(newDetail);
     newDetail.textContent = ("name:" + objectMade.name + "-" + "email:" + objectMade.email + "-" + "phone:" + objectMade.phone + " ");
     newDetail.appendChild(deleteButton);
+    newDetail.appendChild(editButton);
+
     newDetail.setAttribute('email', email);
+    newDetail.setAttribute('name', nameGiven);
+    newDetail.setAttribute('phone', phone);
+    
     allUserDetailList.appendChild(newDetail);
 
     deleteButton.addEventListener("click", deleteFunctionality);
+    editButton.addEventListener('click',editFunctionlity)
 
     localStorage.setItem(email, JSON.stringify(objectMade));
     console.log(localStorage);
